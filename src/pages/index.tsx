e9344c247d4f0481f76040ebc28bce0f6bb829fe
BuzzKill Master <buzzkillmaster@proton.me>
2023-04-11 12:51:32 +0200

    switch to using Lexend as primary fond

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import PageHeader from "@/components/PageHeader";
+import {Lexend} from "next/font/google";
+
+const lexend = Lexend({
+    subsets: ["latin"],
+});
 
 export default function Home() {
     return (
-        <>
+        <div className={lexend.className}>
             <PageHeader />
-        </>
+        </div>
     )
 }
