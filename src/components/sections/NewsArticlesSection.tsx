8318895d6d6cf2d9c04f6ac3512debd440588fc5
BuzzKill Master <buzzkillmaster@proton.me>
2023-04-11 14:14:40 +0200

    switch to grid for consistent margin in news articles to minimise class usage

--- src/components/sections/NewsArticlesSection.tsx
+++ src/components/sections/NewsArticlesSection.tsx
@@ -24,7 +24,7 @@ function NewsArticleContainer(props: {
     sourceUrl: string
 }) {
     return (
-        <article className={"flex flex-col gap-6"}>
+        <article className={"grid gap-6"}>
             <Image src={"https://fakeimg.pl/500"} alt={""} height={500} width={500}/>
             <small>{props.sourceName}</small>
             <h3 className={"text-2xl font-bold"}>{props.title}</h3>
