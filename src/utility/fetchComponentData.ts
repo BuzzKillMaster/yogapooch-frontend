dd1900f1a292e1757028683feaf01b07761ed24f
BuzzKill Master <buzzkillmaster@proton.me>
2023-04-14 17:23:32 +0200

    modify fetch component data to be more flexible

--- src/utility/fetchComponentData.ts
+++ src/utility/fetchComponentData.ts
@@ -1,5 +1,5 @@
-export default async function fetchComponentData(target: string) {
-    const response = await fetch("http://127.0.0.1:1337/api/" + target + "?populate=*", {
+export default async function fetchComponentData(target: string, populate: string = "populate=*") {
+    const response = await fetch("http://127.0.0.1:1337/api/" + target + "?" + populate, {
         method: "GET",
         headers: {
             "Authorization": "Bearer " + process.env.STRAPI_KEY
