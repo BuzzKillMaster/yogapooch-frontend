88a41cf060e4492f7729a2e46d6e8b8127f14029
BuzzKill Master <buzzkillmaster@proton.me>
2023-04-15 06:39:23 +0200

    fetch last piece of static text from api

--- src/app/components/sections/PageHeader.tsx
+++ src/app/components/sections/PageHeader.tsx
@@ -1,8 +1,17 @@
-export default function PageHeader() {
+import fetchComponentData from "@/utility/fetchComponentData";
+
+type ComponentProps = {
+    title: string
+}
+
+export default async function PageHeader() {
+    const {data: {attributes}} = await fetchComponentData("landing-page-header")
+    const props: ComponentProps = attributes
+
     return (
         <header className={"py-8"}>
             <div className="container flex items-center justify-between">
-                <h1 className={"text-4xl font-bold uppercase"}>Yogapooch</h1>
+                <h1 className={"text-4xl font-bold uppercase"}>{props.title}</h1>
             </div>
         </header>
     )
